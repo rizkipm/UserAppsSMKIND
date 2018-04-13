4a58e7f489bf6d5baa0bb501ddcc8ee2bc318ff4
Programmer Muslim <rizki@imastudio.co.id>
2018-04-13 22:11:34 +0700

    add

---
+++ app/src/main/java/superheroku/id/co/appuseridn/network/RestApi.java
@@ -0,0 +1,83 @@
+package superheroku.id.co.kolakaapps.network;
+
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import superheroku.id.co.kolakaapps.modelapi.AllPengumuman.RssJ134AllPengumuman;
+import superheroku.id.co.kolakaapps.modelapi.RssJ104AllInfrastruktur.RssJ104AllInfrastruktur;
+import superheroku.id.co.kolakaapps.modelapi.RssJ104AllPimpinan.RssJ104AllPimpinan;
+import superheroku.id.co.kolakaapps.modelapi.RssJ104Profil.RssJ104Profil;
+import superheroku.id.co.kolakaapps.modelapi.RssJ113Menu.RssJ113Menu;
+import superheroku.id.co.kolakaapps.modelapi.RssJ113Slider.RssJ134Slider;
+import superheroku.id.co.kolakaapps.modelapi.RssJ113Wisata.RssJ113Wisata;
+import superheroku.id.co.kolakaapps.modelapi.RssJ273GetAllIndikator.RssJ54GetAllIndikator;
+import superheroku.id.co.kolakaapps.modelapi.indikatorbyId.RssJ544IndikatorByTipe;
+
+/**
+ * Created by Blackswan on 9/12/2017.
+ */
+
+public interface RestApi {
+
+
+
+    @GET("menu/json")
+    Call<RssJ134Slider> getMenuHome();
+
+    @GET("wisata/json")
+    Call<RssJ113Wisata> getMenuWisata();
+
+    @GET("getAllTipeIndikator")
+    Call<RssJ54GetAllIndikator> getAllTipeIndikator();
+
+
+    @GET("getAllInfrastruktur")
+    Call<RssJ104AllInfrastruktur> getAllInfrastruktur();
+
+    @GET("getAllProfil")
+    Call<RssJ104Profil> getAllProfil();
+
+    @GET("getAllInfoPengumuman")
+    Call<RssJ134AllPengumuman> getAllInfoPengumuman();
+
+
+    @GET("getAllPimpinan")
+    Call<RssJ104AllPimpinan> getAllPimpinan();
+
+
+    @FormUrlEncoded
+    @POST("getIndikatorByTipe")
+    Call<RssJ544IndikatorByTipe> getIndikatorByTipe(
+            @Field("id_tipe_indikator") String id_tipe_indikator
+
+    );
+
+
+//    @FormUrlEncoded
+//    @POST("getKatProdukByID")
+//    Call<RssJ182ProdukByKat> getKatProdukByID(
+//            @Field("id_kategori_produk") String id_kategori_produk
+//
+//    );
+
+//    @FormUrlEncoded
+//    @POST("penawaranBarang")
+//    Call<NewRssJ112Penawaran> penawaranBarang(
+//            @Field("id_produk") String id_produk,
+//            @Field("id_user") String id_user,
+//
+//            @Field("id_toko") String id_toko,
+//            @Field("jml_pesanan") String jml_pesanan,
+//            @Field("satuan_qty") String satuan_qty,
+//            @Field("hrg_penawaran") String hrg_penawaran,
+//            @Field("alamat_penawaran") String alamat_penawaran,
+//            @Field("gmbr_penawaran") String gmbr_penawaran,
+//            @Field("userfile") File userfile
+//    );
+
+
+
+}
