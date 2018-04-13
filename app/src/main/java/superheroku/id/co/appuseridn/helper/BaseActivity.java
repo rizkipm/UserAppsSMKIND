7d9027f160b08c81aa156a1ca1a8d82fbab81350
Programmer Muslim <rizki@imastudio.co.id>
2018-04-13 22:25:41 +0700

    addd

--- app/src/main/java/superheroku/id/co/appuseridn/helper/BaseActivity.java
+++ app/src/main/java/superheroku/id/co/appuseridn/helper/BaseActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.animation.AlphaAnimation;
 
+import com.androidquery.AQuery;
+
 
 public class BaseActivity extends AppCompatActivity {
     protected Context c;
