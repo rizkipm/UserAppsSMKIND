f577f40305a26eb0e1c8a6b46362421c4f6106af
Programmer Muslim <rizki@imastudio.co.id>
2018-04-16 22:43:37 +0700

    add

---
+++ app/src/main/java/superheroku/id/co/appuseridn/activity/KontakActivity.java
@@ -0,0 +1,15 @@
+package superheroku.id.co.appuseridn.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import superheroku.id.co.appuseridn.R;
+
+public class KontakActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kontak);
+    }
+}
